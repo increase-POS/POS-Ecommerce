164b47d0362016db73a92f8620916e4bb5a65d4b
git-eng-dina <dinahnemah@gmail.com>
2022-07-21 01:13:08 +0300

    add to cart

--- PosEcommerce/PosEcommerce/Controllers/CartController.cs
+++ PosEcommerce/PosEcommerce/Controllers/CartController.cs
@@ -26,8 +26,80 @@ namespace PosEcommerce.Controllers
             }
           
         }
-        
 
+        [HttpPost]
+        public ActionResult Add(ItemModel item)
+        {
+            CategoryController cc = new CategoryController();
+
+            List<ItemTransferModel> li;
+            ItemTransferModel itemFound=null;
+
+            if (Session["cart"] == null)
+            {
+                li = new List<ItemTransferModel>();
+
+                Session["count"] = 1;
+
+            }
+            else
+            {
+                li = (List<ItemTransferModel>)Session["cart"];
+
+                itemFound = li.Where(x => x.itemUnitId == item.ItemUnitList.FirstOrDefault().itemUnitId).FirstOrDefault();
+                if(itemFound == null)
+                    Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+
+            }
+
+
+            if (itemFound == null)
+            {
+                decimal basicPrice = 0;
+                decimal price = 0;
+
+                int offerId = 0;
+                string discountType = "1";
+                decimal discountValue = 0;
+
+                basicPrice = (decimal)item.ItemUnitList.FirstOrDefault().price;
+                if (item.ItemUnitList.FirstOrDefault().offerId != null && item.ItemUnitList.FirstOrDefault().offerId != 0)
+                {
+                    offerId = (int)item.ItemUnitList.FirstOrDefault().offerId;
+                    discountType = item.ItemUnitList.FirstOrDefault().discountType;
+                    discountValue = (decimal)item.ItemUnitList.FirstOrDefault().discountValue;
+
+                    price = cc.GetdiscountPrice(item.ItemUnitList.FirstOrDefault().discountType, item.ItemUnitList.FirstOrDefault().discountValue, item.ItemUnitList.FirstOrDefault().price);
+                }
+                else
+                    price = basicPrice;
+
+
+
+                li.Add(new ItemTransferModel()
+                {
+                    itemId = item.itemId,
+                    itemUnitId = item.ItemUnitList.FirstOrDefault().itemUnitId,
+                    price = price,
+                    itemUnitPrice = basicPrice,
+                    quantity = 1,
+                    offerId = offerId,
+                    offerValue = discountValue,
+                    offerType = decimal.Parse(discountType),
+                });
+            }
+            else
+                itemFound.quantity++;
+
+            Session["cart"] = li;
+
+            JsonResult result = this.Json(new
+            {
+                cartCount = li.Count(),
+            }, JsonRequestBehavior.AllowGet);
+
+            return result;
+        }
 
 
 
